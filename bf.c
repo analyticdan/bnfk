b33b4532a4e58b822d327d192dbb8cc83fa0e7e3
Daniel Nguyen <analyticdan@gmail.com>
2020-03-11 15:21:12 -0700

    Indentation :(

--- bf.c
+++ bf.c
@@ -13,22 +13,22 @@ label *labels_head, *loop;
 int main (int argc, char **argv) {
   /* Usage: bf file.bf */
   if (argc != 2) {
-  	printf ("Usage: bf [filename]\n");
-  	return -1;
+    printf ("Usage: bf [filename]\n");
+    return -1;
   }
 
   /* Open file to interpret. */
   file = fopen (argv[1], "r");
   if (file == NULL) {
-  	perror ("Error opening file");
-  	return -1;
+    perror ("Error opening file");
+    return -1;
   }
 
   /* Initialize first data cell. */
   cells_head = malloc (sizeof (cell));
   if (cells_head == NULL) {
-  	perror ("Error mallocing");
-  	return -1;
+    perror ("Error mallocing");
+    return -1;
   }
   data = cells_head;
   data->value = 0;
@@ -38,8 +38,8 @@ int main (int argc, char **argv) {
   /* Initialize label stack (for jumping to beginning of loops). */
   labels_head = malloc (sizeof (label));
   if (labels_head == NULL) {
-  	perror ("Error mallocing");
-  	return -1;
+    perror ("Error mallocing");
+    return -1;
   }
   loop = labels_head;
   loop->offset = 0;
@@ -49,83 +49,82 @@ int main (int argc, char **argv) {
   /* Interpret. */
   int c;
   for (c = fgetc(file); c != -1; c = fgetc(file)) {
-  	label *nextloop;
-  	cell *nextcell;
-  	switch (c) {
-  	  case '>':
-  	  	/* Create next cell if it doesn't exist. */
-  	    if (data->next == NULL) {
-  	      nextcell = malloc (sizeof (cell));
-	      if (nextcell == NULL) {
-	        perror ("Error mallocing");
-	        return -1;
-	      }
-	      nextcell->value = 0;
-  	      nextcell->next = NULL;
-  	      nextcell->prev = data;
-	      data->next = nextcell;
+    label *nextloop;
+    cell *nextcell;
+    switch (c) {
+      case '>':
+        /* Create next cell if it doesn't exist. */
+  	if (data->next == NULL) {
+  	  nextcell = malloc (sizeof (cell));
+	  if (nextcell == NULL) {
+	    perror ("Error mallocing");
+	    return -1;
+	  }
+	  nextcell->value = 0;
+          nextcell->next = NULL;
+          nextcell->prev = data;
+          data->next = nextcell;
         }
         data = data->next;
-  	  	break;
-  	  case '<':
-  	    data = data->prev;
-  	  	break;
-  	  case '+':
-  	    data->value += 1;
-  	  	break;
-  	  case '-':
-  	  	data->value -= 1;
-  	  	break;
-  	  case '.':
-  	  	if (fputc (data->value, stdout) == -1) {
-  	  	  perror ("Error writing to stdout");
-  	  	  return -1;
-  	  	}
-  	  	break;
-  	  case ',':
-  	  	c = fgetc (stdin);
-  	  	if (c == -1) {
-  	  	  perror ("Error reading from stdin");
-  	  	  return -1;
-  	  	}
-  	  	data->value = (char) c;
-  	  	break;
-  	  case '[':
-  	    /* Initialize and push loop beginning onto label stack. */
-  	    nextloop = malloc (sizeof (label));
-		if (nextloop == NULL) {
-		  perror ("Error mallocing");
-		  return -1;
-		}
-		if ((nextloop->offset = ftell(file)) == -1) {
-		  perror ("Error getting file position");
-		  return -1;
-		}
-		nextloop->next = NULL;
-		nextloop->prev = loop;
-		loop->next = nextloop;
-		loop = nextloop;
-  	  	break;
-  	  case ']':
-  	  	if (loop->offset == 0) {
-  	  	  perror("Mismatched loop end");
-  	  	  return -1;
-  	  	} else if (data->value == 0) {
-  	  	  /* Continue if current cell has value 0. */
-  	  	  loop = loop->prev;
-  	  	  free(loop->next);
-  	  	  loop->next = NULL;
-  	  	} else if (fseek(file, loop->offset, 0) == -1) {
-  	  	  /* Otherwise, jump to loop beginning. */
-		  perror ("Error seeking");
-		  return -1;
-		}
-  	  	break;
-  	  default:
-  	  	continue;
-  	}
+  	break;
+    case '<':
+      data = data->prev;
+      break;
+    case '+':
+      data->value += 1;
+      break;
+    case '-':
+      data->value -= 1;
+      break;
+    case '.':
+      if (fputc (data->value, stdout) == -1) {
+        perror ("Error writing to stdout");
+        return -1;
+      }
+      break;
+    case ',':
+      c = fgetc (stdin);
+      if (c == -1) {
+        perror ("Error reading from stdin");
+        return -1;
+      }
+      data->value = (char) c;
+      break;
+    case '[':
+      /* Initialize and push loop beginning onto label stack. */
+      nextloop = malloc (sizeof (label));
+      if (nextloop == NULL) {
+        perror ("Error mallocing");
+        return -1;
+      }
+      if ((nextloop->offset = ftell(file)) == -1) {
+        perror ("Error getting file position");
+        return -1;
+      }
+      nextloop->next = NULL;
+      nextloop->prev = loop;
+      loop->next = nextloop;
+      loop = nextloop;
+      break;
+    case ']':
+      if (loop->offset == 0) {
+        perror("Mismatched loop end");
+        return -1;
+      } else if (data->value == 0) {
+        /* Continue if current cell has value 0. */
+        loop = loop->prev;
+        free(loop->next);
+        loop->next = NULL;
+      } else if (fseek(file, loop->offset, 0) == -1) {
+        /* Otherwise, jump to loop beginning. */
+        perror ("Error seeking");
+        return -1;
+      }
+      break;
+    default:
+      continue;
+    }
   }
-
   if (!feof (file)) {
     perror ("Error reading file");
     return -1;
